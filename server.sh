77a80e2827e74a926ecf6d8c127395beabc778e7
Pavel Mochuk <113521948+illustratumq@users.noreply.github.com>
2024-07-17 15:30:42 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -7,8 +7,8 @@
 #sudo ifconfig eth0 netmask 255.255.255.0 &&
 #sudo ifconfig eth0 up
 
-sudo ifconfig eth0 192.168.9.1
-sudo ifconfig eth0 netmask 255.255.255.0
-sudo ifconfig eth0 up
-sudo route add default gw 192.168.9.1
+#sudo ifconfig eth0 192.168.9.1
+#sudo ifconfig eth0 netmask 255.255.255.0
+#sudo ifconfig eth0 up
+#sudo route add default gw 192.168.9.1
 sudo usbipd -D
