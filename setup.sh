b68d6ebd5adaea3840c25ca3a33558aa60ce47a6
Pavel Mochuk <113521948+illustratumq@users.noreply.github.com>
2024-07-17 15:27:05 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 sudo apt-get update -y && sudo apt-get upgrade -y &&
 sudo apt install mc -y &&
-sudo apt install screen && 
 sudo apt install linux-tools-generic -y &&
-sudo apt install linux-tools-6.5.0-1005-raspi -y &&
+sudo apt install linux-tools-$(uname -r) -y &&
 sudo apt install linux-tools-raspi -y &&
 sudo modprobe usbip-core &&
 sudo modprobe usbip-host &&
