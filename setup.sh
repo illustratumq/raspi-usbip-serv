c60304a4c91a8dd78db652ca9ba1e626ff956320
Pavel Mochuk <113521948+illustratumq@users.noreply.github.com>
2024-07-19 12:54:53 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 sudo apt-get update -y && sudo apt-get upgrade -y &&
 sudo apt install mc -y &&
+sudo apt install net-tools -y &&
 sudo apt install linux-tools-generic -y &&
 sudo apt install linux-tools-$(uname -r) -y &&
 sudo apt install linux-tools-raspi -y &&
